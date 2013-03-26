7688d01ec9908d1038f7552139eac73f555692bd
slima4 <slima4@ukr.net>
2013-03-26 20:26:17 +0200

    begin

---
+++ mainwindow.cpp
@@ -0,0 +1,72 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QFileSystemModel>
+#include <QScrollBar>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    QFileSystemModel *model = new QFileSystemModel(this);
+    model->setRootPath("");
+
+    ui->treeView_1->setModel(model);
+    ui->treeView_2->setModel(model);
+    ui->treeView_1->hideColumn(0);
+
+    connect(ui->treeView_2->verticalScrollBar(), SIGNAL(valueChanged(int)),
+            ui->treeView_1->verticalScrollBar(), SLOT(setValue(int)));
+
+    connect(ui->treeView_2, SIGNAL(expanded(QModelIndex)), SLOT(onExpanded(QModelIndex)));
+    connect(ui->treeView_2, SIGNAL(collapsed(QModelIndex)), SLOT(onCollapsed(QModelIndex)));
+
+
+    ui->treeView_2->setHorizontalScrollMode(QAbstractItemView::ScrollPerPixel);
+    ui->treeView_2->setVerticalScrollMode(QAbstractItemView::ScrollPerPixel);
+    ui->treeView_1->setVerticalScrollMode(QAbstractItemView::ScrollPerPixel);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::setValue(int value)
+{
+    ui->treeView_1->verticalScrollBar()->setValue(value);
+}
+
+void MainWindow::onExpanded(const QModelIndex &model)
+{
+    ui->treeView_1->expand(model);
+}
+
+void MainWindow::onCollapsed(const QModelIndex &model)
+{
+    ui->treeView_1->collapse(model);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
